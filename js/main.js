e863d81ca6544cf11b4a57dcdf8f9bfc5c0f3823
Dmitry <RadionovDmitry93@mail.ru>
2021-09-30 14:58:27 +0500

    Вносит правки функций, согласно рекомендациям наставника

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-function getRandomIntInclusive (min, max) {
-  if (min < max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+function getRandomIntInclusive (from = 'передаваемое ПЕРВОЕ число', to = 'не должно быть <= СЛЕДУЮЩЕГО' ) {
+  if (from < to) {
+    const min = Math.floor(Math.min(from, to));
+    const max = Math.floor(Math.max(from, to));
     return Math.floor(Math.random() * (max - min + 1)) + min; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   }
-  return 'Аргумент max не может быть меньше или равен min!';
+  return `${from  }, ${  to  }!`;
 }
 getRandomIntInclusive();
 
 function checkLengthString (stringName, maxLength) {
-  if (stringName.length <= maxLength) {
-    return true;
-  }
-  return false;
+  return stringName.length <= maxLength;
 }
 checkLengthString();
