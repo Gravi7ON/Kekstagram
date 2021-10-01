d612067069e9f54d182e91757310a6ea99e8969e
Dmitry <RadionovDmitry93@mail.ru>
2021-10-01 23:30:05 +0500

    Обновляет правки, внесеные ранее в функцию рандомайзер

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-function getRandomIntInclusive (from = 'передаваемое ПЕРВОЕ число', to = 'не должно быть >= СЛЕДУЮЩЕГО' ) {
-  if (from < to) {
-    const min = Math.floor(Math.min(from, to));
-    const max = Math.floor(Math.max(from, to));
-    return Math.floor(Math.random() * (max - min + 1)) + min; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive (from, to) {
+  if (from < to && to !== 0 && from >= 0) {
+    return Math.floor(Math.random() * (to - from + 1)) + from;
   }
-  return `${from  }, ${  to  }!`;
+  return 'Аргументы не должны быть отрицательными, а также from не может быть больше или равно to';
 }
 getRandomIntInclusive();
 
