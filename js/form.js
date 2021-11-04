f49c0b6561e20badb8b2e6f9a4ec2da0c0f1fb5a
Dmitry <RadionovDmitry93@mail.ru>
2021-11-04 15:27:11 +0500

    Добавляет валидацию хештегов

--- js/form.js
+++ js/form.js
@@ -1,15 +1,45 @@
 import {isEscapeKey, checkStringLength} from './util.js';
 
+const LIMIT_HASHTAG_LENGTH = 20;
+const LIMIT_HASHTAGS_LENGTH = 5;
 const LIMIT_COMMENT_LENGTH = 140;
 
-const userImageForm = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
+const userImageForm = document.querySelector('.img-upload__overlay');
 const userImageLoad = document.querySelector('#upload-file');
 const buttonCloseImageForm = userImageForm.querySelector('#upload-cancel');
-//const hashtagInput = userImageForm.querySelector('.text__hashtags');
 const textDescriptionInput = userImageForm.querySelector('.text__description');
-//const hashtagExpression = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 const userImage = userImageForm.querySelector('.img-upload__user');
+const hashTagExpression = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const hashTagInput = userImageForm.querySelector('.text__hashtags');
+
+const checkHashTag = () => {
+  const valueHashTag = hashTagInput.value.trim().toLowerCase();
+  const valueHashTags = valueHashTag.split(' ');
+  const hashTagsUniq = [...new Set(valueHashTags)];
+
+  if (valueHashTags.length > LIMIT_HASHTAGS_LENGTH) {
+    hashTagInput.setCustomValidity('Не более 5 хеш-тегов');
+  } else if (valueHashTags.length > hashTagsUniq.length) {
+    hashTagInput.setCustomValidity('Хеш-теги не должны повторяться');
+  } else {
+    hashTagInput.setCustomValidity('');
+  }
+
+  valueHashTags.forEach((tag) => {
+    if (tag === '#') {
+      hashTagInput.setCustomValidity('Хеш-тег не может состоять только из одного символа #');
+    } else if (!tag.startsWith('#')) {
+      hashTagInput.setCustomValidity('Хеш-тег начинается с символа #');
+    } else if (tag.length > LIMIT_HASHTAG_LENGTH) {
+      hashTagInput.setCustomValidity(`Удалите лишние ${ valueHashTag.length - LIMIT_HASHTAG_LENGTH} симв.`);
+    } else if (!hashTagExpression.test(tag)) {
+      hashTagInput.setCustomValidity('Хеш-тег не содержит пробелы, спецсимволы (@, $...), тире, дефис, запятая, эмодзи ...');
+    }
+  });
+
+  hashTagInput.reportValidity();
+};
 
 const checkTextDescription = () => {
   if (!checkStringLength(textDescriptionInput.value, LIMIT_COMMENT_LENGTH)) {
@@ -38,6 +68,7 @@ function showFormEditImage () {
   document.addEventListener('keydown', onformEditEscKeydown);
   buttonCloseImageForm.addEventListener('click', onButtonCloseClick);
   textDescriptionInput.addEventListener('input', checkTextDescription);
+  hashTagInput.addEventListener('input', checkHashTag);
 }
 
 function closeFormEditImage () {
@@ -45,6 +76,7 @@ function closeFormEditImage () {
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onformEditEscKeydown);
   buttonCloseImageForm.removeEventListener('click', onButtonCloseClick);
+  hashTagInput.addEventListener('input', checkHashTag);
   userImageLoad.value = '';
 }
 
